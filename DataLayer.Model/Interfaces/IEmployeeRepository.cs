ff90515a44d211e1fa6f6a05650b6cadcd179feb
AshTech19 <84324865+AshTech19@users.noreply.github.com>
2022-12-19 08:31:13 -0500

    feat: Employee Repository

---
+++ DataLayer.Model/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Model.Models;
+using System.Collections.Generic;
+
+
+namespace DataAccessLayer.Model.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetByCode(string employeeCode);
+        bool SaveEmployee(Employee employee);
+        
+    }
+}
